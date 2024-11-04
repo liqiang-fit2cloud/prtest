5b710b6e5c49da316e4db8a1bb7917c6fabdb4f4
liqiang-fit2cloud <liqiang@fit2cloud.com>
2024-11-04 15:05:40 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello" + " World!");
-        System.out.println("Hello" + " World!");
-        System.out.println("Hello" + " World!");
-        System.out.println("Hello" + " World!");
-        System.out.println("Hello" + " World!");
-        System.out.println("Hello" + " World!");
-        System.out.println("Hello" + " World!");
+        int number1 = 10;
+        int number2 = 20;
+        int sum = add(number1, number2);
+        System.out.println("The sum is: " + sum);
+    }
+ 
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
