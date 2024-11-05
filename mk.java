484b955b667c325f82b8a8ff83a542a5c5653823
liqiang-fit2cloud <liqiang@fit2cloud.com>
2024-11-05 10:18:45 +0800

    a

---
+++ mk.java
@@ -0,0 +1,13 @@
+public class mk {
+    public static void main(String[] args) {
+        // 原始字符串
+        String originalString = "Hello, World! World is beautiful.";
+
+        // 替换 "World" 为 "Java1"
+        String replacedString = originalString.replace("World", "Java");
+
+        // 输出结果
+        System.out.println("原始字符串: " + originalString);
+        System.out.println("替换后的字符串11: " + replacedString);
+    }
+}
